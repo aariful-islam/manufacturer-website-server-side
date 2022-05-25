ef2dccaf83ec03e329a61e6985628abb2c9e23e2
arif99050 <arifulislam99050@gmail.com>
2022-05-25 20:12:55 +0600

    get data from mongodb

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const cors=require('cors')
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqdp4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+ async function run(){
+     try{
+         await client.connect();
+        const toolsCollection = client.db('car-engineer').collection('tools')
+        app.get('/tools', async(req,res)=>{
+            const query={};
+            const cursor=toolsCollection.find(query);
+            const tools=await cursor.toArray();
+            res.send(tools);
+        })
+
+     }
+     finally{
+
+     }
+
+ }
+
+ run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Hello!')
 })
