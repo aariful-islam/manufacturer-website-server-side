94d581fd4fce65f9d8e4ffda4331d06160121c59
arif99050 <arifulislam99050@gmail.com>
2022-05-28 14:45:35 +0600

    send jwt to back end for verification

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqdp4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+function verifyJWT(req,res,next){
+    const authHeader =req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:'unauthorized access'})
+    }
+    const token=authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
+        if(err){
+            return res.status(403).send({message:'forbidden access'})
+        }
+        req.decoded=decoded;
+        next();
+    })
+}
 async function run() {
     try {
         await client.connect();
@@ -87,8 +100,7 @@ async function run() {
           });
           app.get('/orders', async (req,res)=>{
             const email=req.query.email;
-           
-            
+          
             const query={email:email}
             const orders= await orderCollection.find(query).toArray();
             res.send(orders);
