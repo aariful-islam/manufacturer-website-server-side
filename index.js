30ab553a412ff113f5fc54f5fa618b7d134806b5
arif99050 <arifulislam99050@gmail.com>
2022-05-28 16:53:25 +0600

    make admin

--- index.js
+++ index.js
@@ -136,6 +136,27 @@ async function run() {
             const rev= await cursor.toArray();
             res.send(rev)
         })
+        app.get('/user', verifyJWT, async(req,res)=>{
+            const users=await userCollection.find().toArray();
+            res.send(users)
+        })
+
+        app.put('/user/admin/:email',async(req,res)=>{
+            const email=req.params.email;
+            
+            const filter={email:email}
+            
+            const updateDoc={
+                $set:{role:'admin'},
+                
+            };
+            const result = await userCollection.updateOne(filter,updateDoc);
+           
+
+            res.send(result);
+           
+
+        })
     
         app.put('/user/:email',async(req,res)=>{
             const email=req.params.email;
@@ -153,10 +174,7 @@ async function run() {
            
 
         })
-        app.get('/user', async(req,res)=>{
-            const users=await userCollection.find().toArray();
-            res.send(users)
-        })
+        
        
 
     }
